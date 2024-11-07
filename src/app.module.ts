df784d2aa86264c71009a49c35ef2c62e3afb140
Brando Vidal <yisusdracon@gmail.com>
2024-11-06 20:33:45 -0500

    feat: install nestjs config to load env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { PeopleModule } from './people/people.module';
 
@@ -6,7 +7,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PeopleModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PeopleModule],
   controllers: [AppController],
   providers: [AppService],
 })
